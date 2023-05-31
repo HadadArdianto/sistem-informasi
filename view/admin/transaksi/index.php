5380cf658725c13231e93b5cc9ac64b19fa52417
Agung perdana <myagungperdana@gmail.com>
2023-05-31 18:29:00 +0700

    revisi text kas header

--- view/admin/transaksi/index.php
+++ view/admin/transaksi/index.php
@@ -14,9 +14,9 @@ get_sidebar_page();
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Data Pasien</h3>
+                    <h3>Header Kas</h3>
                     <p class="text-subtitle text-muted">
-                        Daftar semua data pasien klinik ini
+                        Form input transaksi kas
                     </p>
                     <?php
                     if (isset($_GET['msg'])) {
